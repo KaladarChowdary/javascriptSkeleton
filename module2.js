2694c9d6f7015eda254966376190e4e9b22ba50e
Kaladar <ckaladar@gmail.com>
2022-07-15 07:22:58 +0530

    New file module2.js, wrote few code about Math object. Will complete remaining tomarrow

---
+++ module2.js
@@ -0,0 +1,23 @@
+//Learning about Math object
+
+"use strict";
+
+console.log(`--------Math Object Properties--------`);
+
+console.log(Math.E);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+console.log(Math.PI);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+
+console.log(`--------Math Object Methods--------`);
+console.log(`Math.trunc(3.9) = ${Math.trunc(3.9)}`);
+console.log(`Math.trunc(3.1) = ${Math.trunc(3.1)}`);
+console.log(`Math.sqrt(4) = ${Math.sqrt(4)}`);
+console.log(`Math.sign(4) = ${Math.sign(4)}`);
+console.log(`Math.sign(-4) = ${Math.sign(-4)}`);
+console.log(`Math.round(3.9) = ${Math.round(3.9)}`);
+console.log(`Math.round(3.1) = ${Math.round(3.1)}`);
